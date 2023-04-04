e19714029a25dde1ee6a73f5e88963ecc88518ed
otherworldindhu <80580975+otherworldindhu@users.noreply.github.com>
2023-04-04 19:53:27 +0530

    .

--- demo.py
+++ demo.py
@@ -1,2 +1,20 @@
 import streamlit as st
 st.title("HEllo streamlit")
+
+st.header("Header")
+
+st.subheader("Sub header")
+
+st.text("Arpudhamm")
+
+st.markdown(""" # h1 tag #h2 tag #h3tag 
+:smile:<br>
+:icecream:
+:butterfly:<br> 
+**bold** _italic_ 
+""",True)
+
+st.latex(r''' a + a r^2 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)''')
+
